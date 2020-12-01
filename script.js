48749c8b45463b6437f7f1c4f7db545af89e3875
Some Guy <byarrley@gmail.com>
2020-12-01 01:46:30 +0000

    Checkpoint

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //User inputs
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseFloat(document.getElementById("years").value);
     
+    //Input validation
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        return;
+    }
+
+    //Computations
+    var interest = principal * years * rate /100;
+    var total = principal + interest;
+    var currentYear = new Date().getFullYear();
+    var futureYear = currentYear + years;
+    
+    //Set result
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark> at an interest rate of <mark>" + rate + "%</mark>.  You will receive an amount of <mark>" + total + "</mark>, in the year <mark>" + futureYear + "</mark>";
 }
-        
+
+function displayRateValue() {
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rate-val").innerHTML = rate + "%";
+}
